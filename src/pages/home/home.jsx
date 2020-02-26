b0effb9f57e673e8672de8fe56f00e19f2433e9f
liubaterehova <liubov.terehova@gmail.com>
2020-02-26 13:04:52 +0000

    Footer

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Footer } from '../../components/footer/footer';
 import { Header } from '../../components/header/header';
 
 import './home.scss';
@@ -7,6 +8,7 @@ import './home.scss';
 export const HomePage = () => (
   <div className="container">
     <Header />
+    <Footer />
   </div>
 );
 
