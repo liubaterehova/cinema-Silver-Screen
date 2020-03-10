d888f25462d4b3a99775122d5d75af53eaa260f6
liubaterehova <liubov.terehova@gmail.com>
2020-03-11 00:17:21 +0300

    fix(client): added new R

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -32,21 +32,55 @@ export const filters = handleActions(
   }, defaultState,
 );
 
-const selectedMenu = [{ city: 'Витебск' }];
+const selectedMenu = [{ city: 'Витебск' }, { date: 'завтра' }];
+
+const sorted = [];
+
+// const filterByCityDate = R.forEach((film) => {
+//   selectedMenu.every(item => {
+//     const sortFilm = R.where({
+//       city: R.equals(item.city),
+//       date: R.equals(item.date),
+//     });
+
+//     if (sortFilm(film)) {
+//       sorted.push(film);
+//     }
+//   });
+// }, films);
+
+
+// const filterByCityDate = R.forEach((film) => {
+//   selectedMenu.every(item => {
+//     const sortFilm = R.whereEq({
+//       {...selectedMenu}
+//     });
+
+//     if (sortFilm(film)) {
+//       sorted.push(film);
+//     }
+//   });
+// }, films);
+
+const selectedMenu = { city: null,  date: 'завтра', from: 13, to: 22 };
 
 const sorted = [];
 
 const filterByCityDate = R.forEach((film) => {
-  selectedMenu.every(item => {
-    const val = R.where({
-      city: R.equals(item.city),
-    });
-
-    if (val(film)) {
+    const sortFilm = R.where({
+      city: (city) => selectedMenu.city === city || selectedMenu.city === null,
+      date: R.equals(selectedMenu.date),
+      hour: R.both(R.gt(R.__, selectedMenu.from),  R.lte(R.__, selectedMenu.to))
+    }
+    );
+    if (sortFilm(film)) {
       sorted.push(film);
     }
-  });
-}, films);
+
+});
+filterByCityDate(films);
+
+console.log('sorted', sorted);
 
 console.log('sorted', sorted.length);
 // const tes = R.forEach((item) => {
