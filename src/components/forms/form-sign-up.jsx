1f1d5b75aa09b354649de1a0b619c21e36006db3
Liuba <liubov.terehova@gmail.com>
2020-03-11 18:19:54 +0300

    fix(client): arr move to function in form-sign-up

--- src/components/forms/form-sign-up.jsx
+++ src/components/forms/form-sign-up.jsx
@@ -3,81 +3,85 @@ import { Form } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { FormElement } from './form-element';
 
-const makeElementsOfFormSignUp = (watch) => [
-  {
-    id: 0,
-    name: 'name',
-    label: 'Ваше имя',
-    placeholder: 'имя',
-    type: 'text',
-    rulesValidation: { required: true },
-    errorMessage: 'Please enter name',
-  },
-  {
-    id: 1,
-    name: 'number',
-    label: 'Номер телефона',
-    placeholder: 'number',
-    type: 'number',
-    rulesValidation: { required: true, minLength: 8 },
-    errorMessage: 'Please enter number',
-  },
-  {
-    id: 2,
-    name: 'signUpEmail',
-    label: 'Email',
-    placeholder: 'email',
-    type: 'email',
-    rulesValidation: { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ },
-    errorMessage: 'Please enter correct email',
-  },
-  {
-    id: 3,
-    name: 'signUpPassword',
-    label: 'Пароль',
-    placeholder: 'password',
-    type: 'password',
-    rulesValidation: { required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/ },
-    errorMessage: 'Please enter password that contain at least 1 lowercase, 1 uppercase, 1 numeric, minimum  8 symbols',
-  },
-  {
-    id: 4,
-    name: 'signUpPasswordRepeat',
-    label: 'Повторите пароль',
-    placeholder: 'password',
-    type: 'password',
-    rulesValidation: {
-      required: true,
-      validate: value => value === watch('signUpPassword'),
-    },
-    errorMessage: 'Please repeat password',
-  },
-];
-
 export const FormSignUp = ({
   errors, onSubmit, register, control, watch,
-}) => (
-  <Form onSubmit={onSubmit}>
-    {makeElementsOfFormSignUp(watch).map(({
-      id, name, label, placeholder, type, errorMessage, rulesValidation,
-    }) => (
-      <FormElement
-        id={id}
-        rules={rulesValidation}
-        key={id}
-        name={name}
-        label={label}
-        placeholder={placeholder}
-        type={type}
-        error={errors[name]}
-        errorMessage={errorMessage}
-        register={register}
-        control={control}
-        watch={watch}
-      />
-    ))}
-  </Form>
-);
+}) => {
+  const makeElementsOfFormSignUp = () => [
+    {
+      id: 0,
+      name: 'name',
+      label: 'Ваше имя',
+      placeholder: 'имя',
+      type: 'text',
+      rulesValidation: { required: true },
+      errorMessage: 'Please enter name',
+    },
+    {
+      id: 1,
+      name: 'number',
+      label: 'Номер телефона',
+      placeholder: 'number',
+      type: 'number',
+      rulesValidation: { required: true, minLength: 8 },
+      errorMessage: 'Please enter number',
+    },
+    {
+      id: 2,
+      name: 'signUpEmail',
+      label: 'Email',
+      placeholder: 'email',
+      type: 'email',
+      rulesValidation: { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ },
+      errorMessage: 'Please enter correct email',
+    },
+    {
+      id: 3,
+      name: 'signUpPassword',
+      label: 'Пароль',
+      placeholder: 'password',
+      type: 'password',
+      rulesValidation: { required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/ },
+      errorMessage: 'Please enter password that contain at least 1 lowercase, 1 uppercase, 1 numeric, minimum  8 symbols',
+    },
+    {
+      id: 4,
+      name: 'signUpPasswordRepeat',
+      label: 'Повторите пароль',
+      placeholder: 'password',
+      type: 'password',
+      rulesValidation: {
+        required: true,
+        validate: value => value === watch('signUpPassword'),
+      },
+      errorMessage: 'Please repeat password',
+    },
+  ];
+
+  return (
+  // put arr into Form cause i need "watch" function from library react-hook-form
+
+    <Form onSubmit={onSubmit}>
+      {makeElementsOfFormSignUp(watch).map(({
+        id, name, label, placeholder, type, errorMessage, rulesValidation,
+      }) => (
+        <FormElement
+          id={id}
+          rules={rulesValidation}
+          key={id}
+          name={name}
+          label={label}
+          placeholder={placeholder}
+          type={type}
+          error={errors[name]}
+          errorMessage={errorMessage}
+          register={register}
+          control={control}
+          watch={watch}
+        />
+      ))}
+    </Form>
+  );
+};
 
 FormSignUp.propTypes = {
   watch: PropTypes.func.isRequired,
@@ -89,4 +93,3 @@ FormSignUp.propTypes = {
   errors: PropTypes.any.isRequired,
   // errors: PropTypes.shape({ signInEmail: PropTypes.object, signUpEmail: PropTypes.object }).isRequired,
 };
-
