ed94a6125287921792446cd101af84c0181186f9
liubaterehova <liubov.terehova@gmail.com>
2020-03-18 10:44:41 +0300

    fix(client): fixed

--- resources/webpack/webpack.client.js
+++ resources/webpack/webpack.client.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
